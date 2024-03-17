c567e617ba78c85c591ce5a9740337c6cc0f2b49
AllhailLesha <a.goponenko0098@gmail.com>
2024-03-17 14:13:33 +0300

    add plugins for scss

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import windiCSSPlugin from 'vite-plugin-windicss'; // Импортируем плагин для работы с Sass
 
 export default defineConfig({
-  base: 'positivus2.0'
+  base: 'positivus2.0',
+  plugins: [
+    vanillaExtractPlugin(), // Добавляем плагин для Vanilla JS
+    windiCSSPlugin(), // Добавляем плагин для работы с Sass
+  ],
 })
